bb3d27e1faa4b6a0522d947b561394ad14989b87
trc96 <toddcrandall1996@gmail.com>
2022-04-08 10:44:10 -0600

    Initial Commit, localhost:3000 working and manual tests done.

---
+++ functions.test.ts
@@ -0,0 +1,8 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    
+    test('shuffleArray should return an array', () => {
+        expect(shuffleArray).toReturnWith([])
+    })
+})
